518aa6c7c69a57cce0711f1313bc62c62c2949be
Andrei Voicu <Andrei.Voicu@softwire.com>
2024-07-08 15:22:09 +0300

    bookish-added user token

--- bookish/controllers/bookish.py
+++ bookish/controllers/bookish.py
@@ -1,6 +1,16 @@
 from flask import request
 from bookish.models.example import Example
+from bookish.models.books import Books
+from bookish.models.users import Users
+from bookish.models.checked_out_books import Checked_Out_Books
 from bookish.models import db
+
+import hashlib
+
+
+def hashing_algorithm(string):
+    result = hashlib.md5(string.encode('utf-8')).hexdigest()
+    return result
 
 
 def bookish_routes(app):
@@ -29,3 +39,41 @@ def bookish_routes(app):
                     'data2': example.data2
                 } for example in examples]
             return {"examples": results}
+
+    @app.route('/signin', methods=['GET'])
+    def handle_signin():
+        try:
+            data = request.get_json()
+            all_users = Users.query.all()
+            current_user = Users(user_name=data['user_name'], user_password=hashing_algorithm(data['user_password']))
+            for user in all_users:
+                if user.user_name == current_user.user_name and user.user_password == current_user.user_password:
+                    user.create_token()
+                    db.session.commit()
+                    return {"status": "OK", "user_token" : user.user_token}
+            return {"error": "Wrong credentials"}
+        except Exception as e:
+            return {"error": str(e)}
+
+    @app.route('/showall', methods=['POST', 'GET'])
+    def handle_showall():
+        try:
+            user_name = request.args.get('user_name')
+            data = Users.query.all()
+            results = [user.serialize() for user in data]
+            return results
+        except Exception as e:
+            return {"error": str(e)}
+
+    @app.route('/signup', methods=['POST'])
+    def handle_signup():
+        if not request.is_json:
+            return {"error": "The request payload is not in JSON format"}
+        try:
+            data = request.get_json()
+            new_user = Users(user_name=data['user_name'], user_password=hashing_algorithm(data['user_password']))
+            db.session.add(new_user)
+            db.session.commit()
+            return {"message": "User has been created successfully."}
+        except Exception as e:
+            return {"error": str(e)}
--- bookish/models/users.py
+++ bookish/models/users.py
@@ -1,5 +1,7 @@
 from bookish.app import db
-
+import datetime
+import hashlib
+from random import randint
 class Users(db.Model):
 
     __tablename__ = 'Users'
@@ -7,20 +9,50 @@ class Users(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_name = db.Column(db.String(), unique=True, nullable=False)
     user_password = db.Column(db.String(), nullable=False)
+    user_token = db.Column(db.String())
+    user_token_expire = db.Column(db.DateTime())
 
     # relations
-   # checked_out_books = db.relationship('Checked_Out_Books', backref='Users', uselist=False)
+    checked_out_books = db.relationship('Checked_Out_Books', backref='Users', uselist=False)
 
     def __init__(self, user_name, user_password):
         self.user_name = user_name
         self.user_password = user_password
 
     def __repr__(self):
-        return f"{self.user_id}: {self.user_name}"
+        return f"{self.user_id}: {self.user_name}, {self.user_password}"
 
     def serialize(self):
         return {
             'user_id': self.user_id,
             'user_name': self.user_name,
-            'user_password': self.user_password
+            'user_password': self.user_password,
+            'user_token': self.user_token,
+            'user_token_expire': self.user_token_expire,
         }
+
+    @staticmethod
+    def get_user_by_token(token):
+        """
+        :param token:
+        :return: true -> token valid
+                 false -> token expired/invalid
+        """
+
+        all_users = Users.query.all()
+        for user in all_users:
+            if user.user_token == token and user.user_token_expire < datetime.datetime.now():
+                return True
+        else:
+            return False
+
+    def create_token(self):
+        """
+
+        :return: token
+        """
+        self.user_token_expire = datetime.datetime.now() + datetime.timedelta(minutes=30)
+        string = str(randint(1000, 9999))
+        self.user_token = hashlib.md5(string.encode('utf-8')).hexdigest()
+
+        return self.user_token
